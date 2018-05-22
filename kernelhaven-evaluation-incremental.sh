a7606676ae7e2420f47ff3f0eef0f1043b42fd9a
Moritz <moritzf@gmail.com>
2018-05-22 15:17:06 +0200

    Update kernelhaven-evaluation-incremental.sh

--- kernelhaven-evaluation-incremental.sh
+++ kernelhaven-evaluation-incremental.sh
@@ -12,12 +12,13 @@ if [ -d .git ]; then
    mv .git .git.backup
 fi
 
+test -d time/incremental || mkdir -p time/incremental
 
 for file in diffs/*.diff; do
     [ -e "$file" ] || continue
     echo "Running on file: $file"
     sed "s/$(echo DIFF_FILE_GENERATED_VALUE | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $file | sed -e 's/[\/&]/\\&/g')/g" config/configuration-incremental.properties > configuration-incremental.properties
-    exec java "-Xms${JVM_MIN_HEAP}" "-Xmx${JVM_MAX_HEAP}" -jar KernelHaven.jar configuration-incremental.properties
+    /usr/bin/time -v -o ./time/incremental/time-$file.log java "-Xms${JVM_MIN_HEAP}" "-Xmx${JVM_MAX_HEAP}" -jar KernelHaven.jar configuration-incremental.properties & disown
 done
 
 if [ -d .git.backup ]; then
